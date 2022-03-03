e4cced30c24781c425f54e48924994cf1319c8a3
Jonathan Tanisaka <jktanisaka@gmail.com>
2022-03-03 02:19:45 +0000

    completed prep-javascript-for-loops exercise

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,9 @@
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+for (let i = 0; i < 10; i++) {
+  console.log(i * 2);
+}
+for (let i = 0; i < 101; i++) {
+  console.log('Time til expolosion: ' + (100 - i));
+}
